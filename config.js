bfe80baeb05c0a338b289b34d88c2df60c835e3d
checkm-lab <edgarsmakwash@gmail.com>
2025-01-03 09:18:11 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "f74kFbhY#6_dpNqZplxmTevVxMSnVVRB4I8UCDtD2BKKpXaxXqXM", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
